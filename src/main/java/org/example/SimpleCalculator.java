a2e89fdc7dcdd7250d1e5a5b4af99a1c21dc6a38
kjm722 <kjm722@naver.com>
2024-11-26 12:13:45 +0900

    TDD Calc

--- src/main/java/org/example/SimpleCalculator.java
+++ src/main/java/org/example/SimpleCalculator.java
@@ -16,4 +16,8 @@ public class SimpleCalculator {
     public int reminder(int num1, int num2){
         return num1%num2;
     }
+
+    public int run(int num1, int num2, int num3) {
+            return ((num1 + num2) * num2 + -num3) * num3 / num2;
+    }
 }
--- src/test/java/SimpleCalculatorTest.java
+++ src/test/java/SimpleCalculatorTest.java
@@ -9,52 +9,10 @@ public class SimpleCalculatorTest {
     SimpleCalculator simpleCalculator = new SimpleCalculator();
 
     @Test
-    @DisplayName("1 * 2 = 2")
-    public void t1(){
-        int rs = simpleCalculator.multiply(1,2);
-        assertThat(rs).isEqualTo(2);
-    }
+    @DisplayName("((3 + 5) * 5 + -10) * 10 / 5 = 60")
+    void t1(){
+        int rs = simpleCalculator.run(3,5,10);
 
-    @Test
-    @DisplayName("1 * 2 = 2")
-    public void t1_f(){
-        int rs = simpleCalculator.multiply(1,3);
-        assertThat(rs).isEqualTo(2);
-    }
-    @Test
-    @DisplayName("1 / 2 = -1")
-    public void t2(){
-        int rs = simpleCalculator.minus(1,2);
-        assertThat(rs).isEqualTo(-1);
-    }
-    @Test
-    @DisplayName("1 / 2 = -1")
-    public void t2_f(){
-        int rs = simpleCalculator.minus(1,3);
-        assertThat(rs).isEqualTo(-1);
-    }
-    @Test
-    @DisplayName("10 / 2 = 5")
-    public void t3(){
-        int rs = simpleCalculator.divide(10,2);
-        assertThat(rs).isEqualTo(5);
-    }
-    @Test
-    @DisplayName("10 / 2 = 5")
-    public void t3_f(){
-        int rs = simpleCalculator.divide(10,3);
-        assertThat(rs).isEqualTo(5);
-    }
-    @Test
-    @DisplayName("10 % 5 = 0")
-    public void t4(){
-        int rs = simpleCalculator.reminder(10,5);
-        assertThat(rs).isEqualTo(0);
-    }
-    @Test
-    @DisplayName("10 % 5 = 0")
-    public void t4_f(){
-        int rs = simpleCalculator.reminder(10,3);
-        assertThat(rs).isEqualTo(0);
+        assertThat(rs).isEqualTo(60);
     }
 }
